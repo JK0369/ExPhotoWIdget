d0d4d15ebae3d579453711783b7e69bb972e120f
jake <palatable7@naver.com>
2022-09-25 20:55:08 +0900

    Initial Commit

---
+++ ExPhotoWidget/ExPhotoWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExPhotoWidgetApp.swift
+//  ExPhotoWidget
+//
+//  Created by 김종권 on 2022/09/25.
+//
+
+import SwiftUI
+
+@main
+struct ExPhotoWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
